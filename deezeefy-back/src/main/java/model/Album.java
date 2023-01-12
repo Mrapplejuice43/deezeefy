874d64b8800639e515bf4851618ba9a43b46bc9a
LoubnaEL06 <loubna.el-hami@etu.univ-cotedazur.fr>
2023-01-12 12:05:35 +0100

    ajout classe abstract MusicList

--- deezeefy-back/src/main/java/model/Album.java
+++ deezeefy-back/src/main/java/model/Album.java
@@ -1,9 +1,52 @@
 package model;
 
-public class Album {
+import java.time.LocalDate;
+
+public class Album  extends MusicList{
+	
+	private String type;// album single, ep
+	
+	
+	
+	
+
+	public Album(Compte compte, int id, String titre, int duree, LocalDate date, int nbContenu, Contenu contenu,
+			String pays, String type) {
+		super(compte, id, titre, duree, date, nbContenu, contenu, pays);
+		this.type = type;
+	}
+
+
+
+
 
 	public Album() {
 		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
+	public String getType() {
+		return type;
+	}
+
+
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Album [type=" + type + "]";
 	}
 
 }
